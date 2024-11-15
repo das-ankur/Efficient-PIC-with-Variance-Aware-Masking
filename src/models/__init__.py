be08638959c1f1fb2f6f43fd7ca143ff4a651cc7
AlbertoPresta <43781044+AlbertoPresta@users.noreply.github.com>
2024-11-15 11:57:16 +0100

    add rems

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,8 +1,8 @@
 from .cnn import WACNN 
 from .pic import VarianceMaskingPIC 
+from .rem_pic import VarianceMaskingPICREM
 
-
-models = {"cnn":WACNN,"pic":VarianceMaskingPIC}
+models = {"cnn":WACNN,"pic":VarianceMaskingPIC,"rem":VarianceMaskingPICREM}
 
 
 
@@ -24,6 +24,24 @@ def get_model(args,device):
                                 total_mu_rep = args.total_mu_rep,
                                 all_scalable = args.all_scalable,
                         )         
+    elif args.model == "rem":
+        net = models[args.model]( N = args.N,
+                                M = args.M,
+                                multiple_decoder = args.multiple_decoder,
+                                multiple_encoder = args.multiple_encoder,
+                                multiple_hyperprior = args.multiple_hyperprior,
+                                dim_chunk = args.dim_chunk,
+                                division_dimension = args.division_dimension,
+                                mask_policy = args.mask_policy,
+                                support_progressive_slices =args.support_progressive_slices,
+                                delta_encode = args.delta_encode,
+                                total_mu_rep = args.total_mu_rep,
+                                all_scalable = args.all_scalable,
+                                check_levels = args.check_levels,
+                                mu_std = args.mu_std,
+                                dimension = args.dimension,
+                        ) 
+
     else:
         raise NotImplementedError
     net = net.to(device)
